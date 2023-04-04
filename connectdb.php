cf9e5bad5472264dcd06aedd8f416015ea43fb57
Bintang Timurlangit <44738387+bintangtimurlangit@users.noreply.github.com>
2023-04-04 11:58:40 +0700

    Update 18

--- connectdb.php
+++ connectdb.php
@@ -2,9 +2,9 @@
 require('debug.php');
 
 $servername = "localhost";
-$username = "root";
-$password_db = "";
-$dbname = "db_ppdb_utswebprog";
+$username = "id20485280_utswebprog";
+$password_db = "Nvl?PDA/GO}e8_Q?";
+$dbname = "id20485280_db_ppdb_utswebprog";
 $conn = mysqli_connect($servername, $username, $password_db, $dbname);
 
 if (!$conn) {
